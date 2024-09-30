71ed0e1a8eb1950505c2887ebbd0d89755c1e411
Elts <elli-maria.kutt@tktk.ee>
2024-09-30 13:28:27 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+//rakendus küsib kasutajal sisestada nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");
+//sting - sõne
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+Console.WriteLine($"Armastan sind" + "!");
